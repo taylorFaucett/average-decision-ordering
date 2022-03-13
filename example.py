01e7f76b24e2b49630221e88368d845019fc4a57
Taylor Faucett <959881+taylorFaucett@users.noreply.github.com>
2022-03-12 21:59:10 -0800

    Fixed array/list type mismatch between ADO and example.py

--- example.py
+++ example.py
@@ -2,11 +2,11 @@ import numpy as np
 
 from average_decision_ordering import calc_ado
 
-data_size = 50000 # Size of the test data set
-n_pairs = 100000 # Maximum number of pairs
-x = np.random.rand(data_size)
-y = np.random.rand(data_size)
-targets = np.random.randint(2, size=data_size)
+data_size = 50000  # Size of the test data set
+n_pairs = 100000  # Maximum number of pairs
+x = list(np.random.rand(data_size))
+y = list(np.random.rand(data_size))
+targets = list(np.random.randint(2, size=data_size))
 
 # ADO calculated without statistics
 print(calc_ado(fx=x, gx=y, target=targets, n_pairs=n_pairs))
